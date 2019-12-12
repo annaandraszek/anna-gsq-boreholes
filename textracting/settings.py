2bdd3a1ee6b4b73b1cce634b9ea91d6a6d0644db
Anna Andraszek <annareach2@gmail.com>
2019-12-12 14:25:38 +1000

    Refactoring get_file functions

--- textracting/settings.py
+++ textracting/settings.py
@@ -12,50 +12,37 @@ def get_report_name(file_id, local_path=False, file_extension=False):
     return file
 
 
-def get_pageinfo_file(file_id, local_path=True):
+def get_file_from_training(folder, file_id, local_path, extension='.json'):
     file = ''
     if local_path:
-        file = 'training/pageinfo/'
-    file += str(file_id) + "_pageinfo.json"
+        file = 'training/' + folder + '/'
+    file += str(file_id) + "_" + folder + extension
     return file
+
+
+def get_pageinfo_file(file_id, local_path=True):
+    return get_file_from_training('pageinfo', file_id, local_path)
+
+
+def get_pagelineinfo_file(file_id, local_path=True):
+    return get_file_from_training('pagelineinfo', file_id, local_path)
 
 
 def get_pagelines_file(file_id, local_path=True):
-    file = ''
-    if local_path:
-        file = 'training/pagelines/'
-    file += str(file_id) + "_pagelines.json"
-    return file
+    return get_file_from_training('pagelines', file_id, local_path)
 
 
 def get_kvs_file(file_id, local_path=True):
-    file = ''
-    if local_path:
-        file = 'training/kvs/'
-    file += str(file_id) + "_kvs.csv"
-    return file
+    return get_file_from_training('kvs', file_id, local_path, extension='.csv')
 
 
 def get_tables_file(file_id, local_path=True):
-    file = ''
-    if local_path:
-        file = 'training/tables/'
-    file += str(file_id) + '_tables.csv'
-    return file
+    return get_file_from_training('tables', file_id, local_path, extension='.csv')
 
 
 def get_text_file(file_id, local_path=True):
-    file = ''
-    if local_path:
-        file = 'training/text/'
-    file += str(file_id) + "_text.txt"
-    return file
+    return get_file_from_training('text', file_id, local_path, extension='.txt')
 
 
 def get_full_json_file(file_id, local_path=True):
-    file = ''
-    if local_path:
-        file = 'training/full_json/'
-    file += str(file_id) + ".json"
-    return file
-
+    return get_file_from_training('full_json', file_id, local_path)
