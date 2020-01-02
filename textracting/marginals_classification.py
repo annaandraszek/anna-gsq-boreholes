1a67d7d27aba1b9c463127e37fc4d6a4f35ba76c
Anna Andraszek <annareach2@gmail.com>
2020-01-02 18:06:28 +1000

    Trying marginals dataset with normalisations - to saving to excel taking very long

--- textracting/marginals_classification.py
+++ textracting/marginals_classification.py
@@ -6,16 +6,17 @@ import settings
 import re
 
 def create_dataset():
-    columns = ['DocID', 'PageNum', 'LineNum', 'Text', 'WordsWidth', 'Width', 'Height', 'Left', 'Top', 'ContainsNum',
-               'ContainsTab', 'ContainsPage', 'Marginal']
+    columns = ['DocID', 'PageNum', 'LineNum', 'NormedLineNum','Text', 'WordsWidth', 'Width', 'Height', 'Left', 'Top', 'ContainsNum',
+               'ContainsTab', 'ContainsPage', 'Centrality', 'Marginal']
     pageinfos = glob.glob('training/restructpageinfo/*')
-    docset = []
+    df = pd.DataFrame(columns=columns)
     for pagesinfo in pageinfos:
+        docset = []
         pi = json.load(open(pagesinfo))
         docid = pagesinfo.split('\\')[-1].replace('_1_restructpageinfo.json', '').strip('cr_')
-
         for info in pi.items():
             page = info[0]
+            lines = len(info[1])
             for line in info[1]:
                 contains_num = 0
                 contains_tab = 0
@@ -27,11 +28,21 @@ def create_dataset():
                     contains_tab = 1
                 if 'page' in line['Text'].lower():
                     contains_page = 1
-                docset.append([docid, int(page), line['LineNum'], line['Text'], line['WordsWidth'],
-                               bb['Width'], bb['Height'], bb['Left'], bb['Top'], contains_num, contains_tab,
-                               contains_page, 0])
+                #normed_line = line['LineNum'] / lines
+                centrality = 0.5 - abs(bb['Left'] + (bb['Width']/2) - 0.5)  # the higher value the more central
 
-    df = pd.DataFrame(data=docset, columns=columns)
+                docset.append([docid, int(page), line['LineNum'], 0, line['Text'], line['WordsWidth'],
+                               bb['Width'], bb['Height'], bb['Left'], bb['Top'], contains_num, contains_tab,
+                               contains_page, centrality, 0])
+
+            temp = pd.DataFrame(data=docset, columns=columns)
+            temp['NormedLineNum'] = (temp['LineNum'] - min(temp['LineNum'])) / (max(temp['LineNum']) - min(temp['LineNum']))
+            df = df.append(temp, ignore_index=True)
+
+    unnormed = np.array(df['Centrality'])
+    normalized = (unnormed - min(unnormed)) / (max(unnormed) - min(unnormed))
+    df['Centrality'] = normalized
+
     return df
 
 
