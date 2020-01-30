633cca1668c9bd58fb3d2f0da4d075920c66add6
Anna Andraszek <annareach2@gmail.com>
2020-01-30 14:53:09 +1000

    Added support for running workflow with cmd line args

--- textracting/workflow.py
+++ textracting/workflow.py
@@ -23,13 +23,42 @@ import pandas as pd
 import time
 import csv
 import datetime
+import argparse
+import numpy as np
+import warnings
+
 
 if __name__ == '__main__':
-    docids = textloading.get_reportid_sample(num=20, cutoffdate=None, rtype_exclude='WELCOM')
+    parser = argparse.ArgumentParser()
+    #parser.add_argument("--id", help="report ID to bookmark", type=str)
+    parser.add_argument("--id", help="report IDs to bookmark", nargs='+')  # list type
+    parser.add_argument("-s", "--sample", help='number of reports to sample', type=int)
+    args = parser.parse_args()
+    mode = 'sample'  # default behaviour is random sampling
+    if args:
+        warnings.filterwarnings("ignore")
+    if args.sample:
+        mode = 'sample'
+        num_sample = args.sample
+
+    if args.id:
+        if len(args.id) == 1:
+            docids = [args.id]
+        else: docids = args.id
+        mode = 'given'
+
+    if mode == 'sample':
+        print("Running in 'sample' mode")
+        if not args.sample:
+            num_sample = 20
+        docids = textloading.get_reportid_sample(num=num_sample, cutoffdate=None, rtype_exclude='WELCOM')
+    else:
+        print("Running in 'given' mode")
+
     training_folders = os.walk('training/QDEX/')
     training_docids = [x[0].split('\\')[-1] for x in training_folders]
     #docids = ['15042', '41275', '4639', '48670', '593', '3051', '24357', '15568', '68677', '48897', '36490', '5261', '44433'] #'41568', '41982', '10189', '102109', '43758', '105472', '48907'
-    print(docids)
+    print("Report IDs to bookmark: ",  docids)
 
     log_file = 'bookmarker_log.csv'
     # log file cols = report_id, time2textract, time2ml, toc, time_run
@@ -44,11 +73,11 @@ if __name__ == '__main__':
             try:
                 textmain.textract(docid, features=['TABLES', 'FORMS'])
             except FileNotFoundError:
-                print ("Report file doesn't exist in S3")
+                print("Report file doesn't exist in S3")
                 continue
             textract_end = time.time()
             textract_time = textract_end - textract_start
-            print("Time to textract: " + str(docid) + " " + str(textract_time))
+            print("Time to textract: " + str(docid) + " " + "{0:.2f}".format(textract_time) + " seconds")
             ml_start = time.time()
             texttransforming.clean_and_restruct(docid)
             report = search_report.Report(docid)  # need every ml method here to be able to create a dataset with an unseen report
@@ -57,8 +86,8 @@ if __name__ == '__main__':
             search_report.save_report_sections(report)
             ml_end = time.time()
             ml_time = ml_end - ml_start
-            print("Time to ML, bookmark, export to text: " + str(ml_time))
-            print("COMPLETED BOOKMARKING " + docid + ", total time: " + str(ml_time + textract_time))
+            print("Time to ML, bookmark, export to text: " + "{0:.2f}".format(ml_time) + " seconds")
+            print("COMPLETED BOOKMARKING " + docid + ", total time: " + "{0:.2f}".format(ml_time + textract_time) + " seconds")
             toc_exists = True if report.toc_page else False
             bookmark_time = datetime.datetime.now()
             with open(log_file, 'a', newline='') as log:
