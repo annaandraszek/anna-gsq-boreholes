587b72e88220e972b4be7218ee928a73f1700e28
Anna Andraszek <annareach2@gmail.com>
2020-01-30 15:34:03 +1000

    Added support for parameter args for sample mode

--- textracting/workflow.py
+++ textracting/workflow.py
@@ -33,6 +33,8 @@ if __name__ == '__main__':
     #parser.add_argument("--id", help="report ID to bookmark", type=str)
     parser.add_argument("--id", help="report IDs to bookmark", nargs='+')  # list type
     parser.add_argument("-s", "--sample", help='number of reports to sample', type=int)
+    parser.add_argument("-c", "--cutoffdate", help="cutoff year for reports to be no older than", type=int)
+    parser.add_argument("-e", "--exclude", help="report types to exclude. must match report type code eg. WELCOM for Well Completion Report")
     args = parser.parse_args()
     mode = 'sample'  # default behaviour is random sampling
     if args:
@@ -40,18 +42,26 @@ if __name__ == '__main__':
     if args.sample:
         mode = 'sample'
         num_sample = args.sample
+        if args.cutoffdate:
+            cutoffdate = pd.Timestamp(args.cutoffdate, 1, 1)
+        else: cutoffdate = None
+        if args.exclude:
+            rtype_exclude = args.exclude
+        else: rtype_exclude = None
 
     if args.id:
-        if len(args.id) == 1:
-            docids = [args.id]
-        else: docids = args.id
+        docids = args.id
         mode = 'given'
 
     if mode == 'sample':
-        print("Running in 'sample' mode")
         if not args.sample:
             num_sample = 20
-        docids = textloading.get_reportid_sample(num=num_sample, cutoffdate=None, rtype_exclude='WELCOM')
+            cutoffdate = None
+            rtype_exclude = 'WELCOM'
+        print("Running in sample mode. Num samples: " + str(num_sample) + " Cutoff date: " + str(cutoffdate) +
+              " Excluding: " + str(rtype_exclude))
+
+        docids = textloading.get_reportid_sample(num=num_sample, cutoffdate=cutoffdate, rtype_exclude=rtype_exclude)
     else:
         print("Running in 'given' mode")
 
@@ -68,7 +78,9 @@ if __name__ == '__main__':
             writer.writerow(['report_id', 'time2textract', 'time2ml', 'toc', 'time_run'])
 
     for docid in docids:
-        if docid not in training_docids:
+        #if docid not in training_docids:
+            # all the below checks also need to check if the --force arg is True, which would overrule their skip
+            # check if textract needs to be run or if fulljson already exists
             textract_start = time.time()
             try:
                 textmain.textract(docid, features=['TABLES', 'FORMS'])
@@ -79,10 +91,13 @@ if __name__ == '__main__':
             textract_time = textract_end - textract_start
             print("Time to textract: " + str(docid) + " " + "{0:.2f}".format(textract_time) + " seconds")
             ml_start = time.time()
+            # check if clean and restruct needs to be run or if restructpageinfo alredy exists
             texttransforming.clean_and_restruct(docid)
+            # check if search report, bookmark report, needs to be run or if bookmarked pdf already exists
             report = search_report.Report(docid)  # need every ml method here to be able to create a dataset with an unseen report
             #search_report.draw_report(report)
             search_report.bookmark_report(report)
+            # check if needs to be run or if sections word doc already exists
             search_report.save_report_sections(report)
             ml_end = time.time()
             ml_time = ml_end - ml_start
