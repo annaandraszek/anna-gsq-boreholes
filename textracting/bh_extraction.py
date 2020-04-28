df9cb29673aa806a3c78367e104f1609ddc9daab
Anna Andraszek <annareach2@gmail.com>
2020-04-28 15:19:57 +1000

    Splitting grid and geo coordinate info in location extraction

--- textracting/bh_extraction.py
+++ textracting/bh_extraction.py
@@ -8,10 +8,10 @@ import numpy as np
 import re
 
 
-table_data_cols = ['BH', 'location1', 'location2',
-                'BH source', 'location1 source', 'location2 source']
+table_data_cols = ['BH', 'grid_loc_1', 'grid_loc_2', 'geo_loc_1', 'geo_loc_2',
+                'BH source', 'grid_loc_1 source', 'grid_loc_2 source', 'geo_loc_1 source', 'geo_loc_2 source']
 bh_data_cols = ['DocID', 'File']
-table_data_cols.extend(bh_data_cols)
+bh_data_cols.extend(table_data_cols)
 bh_col = []
 bh_key = []
 bh_loc_col = []
@@ -25,10 +25,16 @@ def init():
     bh_col_str = 'Hole ID, borehole nu, drillhole, no. hole, hole no., Hole No. (Site No.), bore, hole, well, drill hole, hole #, bore or well reference number, bore or well reference nu, borehole, uphole, holeid, bh_id, well name, viell, bore no'
     bh_key_str = ' hole number, drill hole, well name and, well no, borehole no, well number, Identifying name of the petroleum well, well name and number, well, well name, hole name'
 
-    bh_loc_col_str = 'Survey ea, survey no, surveyed ea, surveyed no, collar (local easting, coordinates grid) northing, northing, easting, Easting AMG (Local), Northing AMG (Local), Co-ords, collar, Local E & N, Co-Ordinates North/East, collar east, collar north, AMG east, AMG north, East AGD66 Zone 54, North AGD66 Zone 54, AMGEASTIN, AMGNORTH, east, north, Easting (m), Northing (m) '
-    bh_loc_key_str = 'Latitude, longitude, northing, easting, surveyed location, lat, long, location, grid (amg), grid location, field location'
+#    bh_loc_col_str = ''
+    bh_geo_col_str = ''
+    bh_grid_col_str = 'Survey ea, survey no, surveyed ea, surveyed no, collar (local easting, coordinates grid) northing, northing, easting, Easting AMG (Local), Northing AMG (Local), Co-ords, collar, Local E & N, Co-Ordinates North/East, collar east, collar north, AMG east, AMG north, East AGD66 Zone 54, North AGD66 Zone 54, AMGEASTIN, AMGNORTH, east, north, Easting (m), Northing (m) '
 
-    strs = [bh_col_str, bh_key_str, bh_loc_col_str, bh_loc_key_str]
+#    bh_loc_key_str = ''
+    bh_geo_key_str = 'Latitude, longitude, lat, long'
+    bh_grid_key_str = 'northing, easting, grid (amg), grid location, surveyed location, location, field location'
+
+
+    strs = [bh_col_str, bh_key_str, bh_geo_col_str, bh_grid_col_str, bh_geo_key_str, bh_grid_key_str]
     arrays = []
 
     for s in strs:
@@ -40,10 +46,14 @@ def init():
     bh_col = arrays[0]
     global bh_key
     bh_key = arrays[1]
-    global bh_loc_col
-    bh_loc_col = arrays[2]
-    global bh_loc_key
-    bh_loc_key = arrays[3]
+    global bh_geo_col
+    bh_geo_col = arrays[2]
+    global bh_grid_col
+    bh_grid_col = arrays[3]
+    global bh_geo_key
+    bh_geo_key = arrays[4]
+    global bh_grid_key
+    bh_grid_key = arrays[5]
 
 
 def save_rows(file_name, df):
@@ -83,17 +93,21 @@ def find_loc_from_key(table, loc_source_i):
 
 
 def verify_loc(loc):
-    loc = str(loc).lower()
-    contains_letters = loc.islower()
+    p_loc = str(loc).lower()
+    contains_letters = p_loc.islower()
 
     if contains_letters:
-        loc = loc[:-1].lower()
-        contains_letters = loc.islower()
+        p_loc = p_loc.strip()  # remove leading and trailing whitespace
+        l_loc = p_loc.replace('amg', '')
+
+        #l_loc = p_loc[:-1].lower()
+        l_loc = l_loc.replace('s', '').replace('n', '').replace('e','').replace('w','').replace('m', '')
+        contains_letters = l_loc.islower()
 
         if contains_letters:
             print('loc ', loc, ' invalid')
             return 'invalid'
-    if not loc:
+    if not p_loc:
         print('loc nan')
         return 'invalid_nan'
 
@@ -133,14 +147,13 @@ def search_right(table, loc_source_i):
 
 
 def extract_from_keys(table):
-    # for location: if the next cell's value after loc source is a float, (without the last char because it could be e or n)
-    loc = 0
     bh_source = None
     bh_source_i = None
-    loc1_source = None
-    loc1_source_i = None
-    loc2_source = None
-    loc2_source_i = None
+    grid_source = []
+    grid_source_i = []
+    geo_source = []
+    geo_source_i = []
+
     cols = pd.DataFrame([table.columns.values], columns=table.columns.values)
     alltable = pd.concat([cols, table], ignore_index=True)  # put "column name" values into the table to be worked with
     for i, row in alltable.iterrows():
@@ -149,16 +162,12 @@ def extract_from_keys(table):
             if cvalue in bh_key:
                 bh_source = cell
                 bh_source_i = [i,j]  # +1, bc assuming value of the key will be next cell accross
-            elif cvalue in bh_loc_key:
-                if loc == 0:
-                    loc1_source = cell
-                    loc1_source_i = [i,j]
-                    loc += 1
-                elif loc == 1:
-                    loc2_source = cell
-                    loc2_source_i = [i,j]
-                else:
-                    print('more than two loc? :', cell)
+            elif cvalue in bh_geo_key:
+                geo_source.append(cell)
+                geo_source_i.append([i,j])
+            elif cvalue in bh_grid_key:
+                grid_source.append(cell)
+                grid_source_i.append([i,j])
     if bh_source:
         try:
             bh = alltable.iloc[bh_source_i[0], bh_source_i[1]+1] # assuming BH name will be to the right, but need to write something as for loc
@@ -169,34 +178,42 @@ def extract_from_keys(table):
             except IndexError:
                 print("can't look right or down for bh")
                 return None
-        loc1 = None
-        loc2 = None
-        if loc1_source:
-            loc1 = find_loc_from_key(alltable, loc1_source_i)
-        if loc2_source_i:
-            loc2 = find_loc_from_key(alltable, loc2_source_i)
+        grid_loc = []
+        geo_loc = []
+        for i in range(len(grid_source)):
+            grid_loc.append([find_loc_from_key(alltable, grid_source_i[i])])
+        for i in range(len(geo_source)):
+            geo_loc.append([find_loc_from_key(alltable, geo_source_i[i])])
 
-        return extracted_to_df(bh, loc1, loc2, bh_source, loc1_source, loc2_source)
+        return extracted_to_df(bh, grid_loc, geo_loc, bh_source, grid_source, geo_source)
     return None
 
 
-def extracted_to_df(bhs, loc1s, loc2s, bh_source, loc1_source, loc2_source):
+def extracted_to_df(bhs, grid_loc, geo_loc, bh_source, grid_source, geo_source):
     if not isinstance(bhs, np.ndarray):
         bhs = [bhs]
-    if not isinstance(loc1s, np.ndarray):
-        loc1s = [loc1s]
-    if not isinstance(loc2s, np.ndarray):
-        loc2s = [loc2s]
+
+    for i in range(2):
+        if len(grid_loc) <= i:
+            grid_loc.append(None)
+            grid_source.append(None)
+        if len(geo_loc) <= i:
+            geo_loc.append(None)
+            geo_source.append(None)
 
     rows = len(bhs)
     print(rows)
-    add_to_df = [bhs, loc1s, loc2s, [bh_source], [loc1_source], [loc2_source]]
+    add_to_df = [bhs, grid_loc[0], grid_loc[1], geo_loc[0], geo_loc[1],
+                 [bh_source], [grid_source[0]], [grid_source[1]], [geo_source[0]], [geo_source[1]]]
     for i in range(len(add_to_df)):
         if add_to_df[i] is None:
             add_to_df[i] = [None for e in range(rows)]
+        #try:
         elif len(add_to_df[i]) < rows:
             if len(add_to_df[i]) == 1:
                 add_to_df[i] = [add_to_df[i][0] for e in range(rows)]
+        #except TypeError:
+        #    print(add_to_df[i])
 
     dfdata = [pd.Series(a) for a in add_to_df]
 
@@ -207,34 +224,32 @@ def extracted_to_df(bhs, loc1s, loc2s, bh_source, loc1_source, loc2_source):
 
 
 def extract_from_columns(table):
-    loc = 0
     bh_source = None
-    loc1_source = None
-    loc2_source = None
+    grid_source = []
+    geo_source = []
     for name in table.columns:
         proc_name = preprocess_str(name)
         if proc_name in bh_col:
             print('bh name: ', name)
             bh_source = name
-        elif proc_name in bh_loc_col:
+        elif proc_name in bh_geo_col:
             print('bh loc name: ', name)
-            if loc == 0:
-                loc1_source = name
-                loc += 1
-            elif loc == 1:
-                loc2_source = name
-            else:
-                print('more than two loc? :', name)
+            #k = len(geo_source)
+            geo_source.append(name)
+        elif proc_name in bh_grid_col:
+            #k  = len(grid_source)
+            grid_source.append(name)
+
     if bh_source:
         bhs = table[bh_source].values
-        loc1s = None
-        loc2s = None
-        if loc1_source:
-            loc1s = table[loc1_source].values
-        if loc2_source:
-            loc2s = table[loc2_source].values
+        grid_loc = []
+        geo_loc = []
+        for i in range(len(grid_source)):
+            grid_loc.append(table[grid_source[i]].values)
+        for i in range(len(geo_source)):
+            geo_loc.append(table[geo_source[i]].values)
 
-        return extracted_to_df(bhs, loc1s, loc2s, bh_source, loc1_source, loc2_source)
+        return extracted_to_df(bhs, grid_loc, geo_loc, bh_source, grid_source, geo_source)
     return None
 
 
@@ -259,12 +274,17 @@ def extract_bh(docid, bh=True):
 
         for table in bhtables:
             res1 = extract_from_columns(table)
+            res2 = extract_from_keys(table)
+            # if isinstance(res1, pd.DataFrame) and isinstance(res2, pd.DataFrame):
+            #     # compare the two and throw one out
+            #     print('comparing')
+            #     res = res1 # placeholder
+            #     bh_data = bh_data.append(res, ignore_index=True)
+            # else:
             if isinstance(res1, pd.DataFrame):
                 bh_data = bh_data.append(res1, ignore_index=True)
-            res2 = extract_from_keys(table)
             if isinstance(res2, pd.DataFrame):
                 bh_data = bh_data.append(res2, ignore_index=True)
-
 
         bh_data['DocID'] = docid
         bh_data['File'] = file
